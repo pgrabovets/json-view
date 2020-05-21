288f52e7d878830c3ed965cdb9903a33ab88c9b7
Pavel Grabovets <pgrabovets3056@gmail.com>
2020-05-21 23:25:36 +0300

    Serve static files from public and dist directories

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('src'));
+app.use(express.static('public'));
+app.use(express.static('dist'));
 
 app.listen(3000, function () {
   console.log('Static server app listening at localhost:3000');
